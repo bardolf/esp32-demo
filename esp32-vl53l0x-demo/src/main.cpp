9858233149d1825d0d798c27a7be3861cab8accb
Milan Nemec <milanemec@gmail.com>
2022-08-26 08:58:50 +0200

    improved example (non blocking)

--- esp32-vl53l0x-demo/src/main.cpp
+++ esp32-vl53l0x-demo/src/main.cpp
@@ -1,39 +1,42 @@
 #include <Arduino.h>
+#include <ArduinoLog.h>
 #include <Wire.h>
+
 #include "Adafruit_VL53L0X.h"
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
 void setup() {
-  Serial.begin(921600);
-  // wait until serial port opens for native USB devices
-  while (! Serial) {
-    delay(1);
-  }
+    Serial.begin(115200);
 
-  Serial.println("Adafruit VL53L0X test.");
-  if (!lox.begin()) {
-    Serial.println(F("Failed to boot VL53L0X"));
-    while(1);
-  }
-  // power
-  Serial.println(F("VL53L0X API Continuous Ranging example\n\n"));
-  pinMode(LED_BUILTIN, OUTPUT);
-  // start continuous ranging
-  lox.setMeasurementTimingBudgetMicroSeconds(20000);
-  lox.startRangeContinuous();
+    // initialize logging
+    while (!Serial && !Serial.available()) {
+    }
+    Log.begin(LOG_LEVEL_INFO, &Serial);
+    Log.setShowLevel(true);
+    Log.infoln("START");
+
+    Log.infoln("Adafruit VL53L0X test.");
+    if (!lox.begin()) {
+        Log.errorln("Failed to boot VL53L0X");
+        while (1)
+            ;
+    }
+
+    // start continuous ranging
+    lox.setMeasurementTimingBudgetMicroSeconds(20000);
+    lox.startRangeContinuous();
 }
 
 void loop() {
-  if (lox.isRangeComplete()) {
-    uint16_t distance = lox.readRange();
-    if (distance < 500) {
-      digitalWrite(LED_BUILTIN, HIGH);
-      Serial.print(micros()/1000);
-      Serial.print(" distance in mm: ");
-      Serial.println(distance);
+    if (lox.isRangeComplete()) {
+        uint16_t distance = lox.readRange();
+        if (distance < 500) {
+            Log.infoln("%d Distance: %d", millis(), distance);
+        } else {
+            Log.infoln("%d No object detected.", millis());
+        }
     } else {
-      digitalWrite(LED_BUILTIN, LOW);
+        Log.noticeln("%d not ready yet.", millis());
     }
-  }
 }
