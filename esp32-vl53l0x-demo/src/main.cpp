dbe17453293f46bf3c0b9b4bbd0e57a274fccbc7
Milan Nemec <milanemec@gmail.com>
2022-08-26 20:00:27 +0200

    vl53l0x demo reported via BT

--- esp32-vl53l0x-demo/src/main.cpp
+++ esp32-vl53l0x-demo/src/main.cpp
@@ -3,8 +3,13 @@
 #include <Wire.h>
 
 #include "Adafruit_VL53L0X.h"
+#include "BluetoothSerial.h"
+
+#define TIMING_BUDGET 30000
+#define RANGE_THRESHOLD 600
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+BluetoothSerial serialBT;
 
 void setup() {
     Serial.begin(115200);
@@ -22,9 +27,12 @@ void setup() {
         while (1)
             ;
     }
+    serialBT.begin("ESP32BT_TEST");  // Bluetooth device name
+
 
     // start continuous ranging
-    lox.setMeasurementTimingBudgetMicroSeconds(20000);
+    lox.setMeasurementTimingBudgetMicroSeconds(TIMING_BUDGET);
+    lox.configSensor(lox.VL53L0X_SENSE_HIGH_SPEED);
     lox.startRangeContinuous();
 }
 
@@ -33,10 +41,12 @@ void loop() {
         uint16_t distance = lox.readRange();
         if (distance < 500) {
             Log.infoln("%d Distance: %d", millis(), distance);
+            String s = String(millis()) + " range " + String(distance) + "mm ";
+            serialBT.println(s);          
         } else {
             Log.infoln("%d No object detected.", millis());
         }
     } else {
-        Log.noticeln("%d not ready yet.", millis());
+        // Log.noticeln("%d not ready yet.", millis());
     }
 }
